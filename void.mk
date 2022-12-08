c682b778029064f2f0f5d7f1f58d9378e218db29
Alita <116470315+TheAtt1la@users.noreply.github.com>
2022-12-08 11:19:10 -0300

    Update void.mk

--- void.mk
+++ void.mk
@@ -1,14 +1,16 @@
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 $(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
-$(call inherit-product, vendor/aosp/config/BoardConfig.mk)
 $(call inherit-product, device/generic/sepolicy/common/sepolicy.mk)
 -include vendor/aosp/build/core/config.mk
-
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
+
 SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
 TARGET_BOOT_ANIMATION_RES := 720
 
+PRODUCT_PACKAGES += \
+    libaptX_encoder \
+    libaptXHD_encoder
